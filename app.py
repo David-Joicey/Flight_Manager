b0b3a94f743bc3d5ff1ee2fbe1db2af10d4acd0b
david <c5021154@newcastle.ac.uk>
2026-06-02 21:05:24 +0100

    Added flight search form to main ('/') route and created html file for search results

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'), #Change key in production
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -13,7 +12,8 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
     os.makedirs(app.instance_path, exist_ok=True)
-    # a simple page that says hello
+
+    #Home page route
     @app.route('/')
     def home():
         return render_template('index.html')
