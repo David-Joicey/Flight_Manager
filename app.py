727847a8e9312fc032ab8927b9d4c964d5704fb5
david <c5021154@newcastle.ac.uk>
2026-06-04 20:07:03 +0100

    Added app.py comments and updated visual layout of existing components

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import os
 from flask import Flask, render_template, request
 from services.mock_flight_api import MockFlightAPI
 
+#Factory Function to create Flask app instance
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'), #Change key in production
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+    #Test configuration toggle
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
@@ -22,6 +24,7 @@ def create_app(test_config=None):
     #Results page route
     @app.route('/results')
     def results():
+        #Gets flight search parameters from the search form
         origin = request.args.get('From')
         destination = request.args.get('To')
         date = request.args.get('date')
