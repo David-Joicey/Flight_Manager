b60ddff9bb0cc76cae56863d33d57a9d8fd530a5
david <c5021154@newcastle.ac.uk>
2026-07-01 19:06:02 +0100

    Added booking list backend

---
+++ booking_list.py
@@ -0,0 +1,22 @@
+from flask import (
+    Blueprint, flash, g, render_template, request, session, url_for
+)
+from database.db import get_db
+from auth import login_required
+
+#Blueprint for booking routes
+bp = Blueprint('bookings', __name__, url_prefix='/bookings')
+
+#Booking route
+@bp.route('/')
+@login_required
+def bookings():
+    db = get_db()
+    #Gets booking history by user id of logged in user
+    bookings = db.execute(
+        'SELECT fnumber, airline, price, origin, destination, atime, dtime '
+        'FROM Bookings WHERE uid = ? ORDER BY date DESC',
+        (g.user['uid'],)
+    ).fetchall()
+
+    return render_template('bookings.html', bookings=bookings)
