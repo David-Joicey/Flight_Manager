b12773bbfccc5a9eb3ec78536d13a105580c13e8
david <c5021154@newcastle.ac.uk>
2026-07-28 17:54:50 +0100

    Added config.py + search_history.py docstring documentation

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+"""
+This module contains configuration settings for the application,
+which is the API key for the AviationStack API used to fetch live flight data.
+It is loaded from environment variables to keep api keys hidden and secure.
+"""
+
 import os
 
 AVIATIONSTACK_API_KEY = os.getenv("AVIATIONSTACK_API_KEY")
--- search_history.py
+++ search_history.py
@@ -1,3 +1,8 @@
+"""
+This module contains the search history route for the application.
+It allows for user-specific search history to be displayed for the currently logged-in user.
+"""
+
 from flask import (
     Blueprint, flash, g, render_template, request, session, url_for
 )
@@ -11,6 +16,16 @@ bp = Blueprint('search_history', __name__, url_prefix='/search_history')
 @bp.route('/')
 @login_required
 def search_history():
+    """
+    Displays the search history for the currently logged-in user.
+    Queries the database for search history records with matching user ID (uid)
+    to the logged-in user and orders them by date (descending).
+    Requires user to be logged in to access.
+
+    Returns:
+        Rendered HTML Jinja2 template "search_history.html" with the user's search history.
+    """
+
     db = get_db()
     #Gets history by user id of logged in user
     searches = db.execute(
